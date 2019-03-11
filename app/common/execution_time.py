a7333e986a1d4ada6b95cda68ce8ef67f415ad65
pancham <panchamrb@phonepe.com>
2019-03-11 17:42:19 +0530

    More logs

--- app/common/execution_time.py
+++ app/common/execution_time.py
@@ -7,7 +7,7 @@ def execution_time(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         start = time()
-        logging.info("--- STARTING EXECUTION of {}---".format(func.__name__))
+        logging.info("--- STARTING EXECUTION of {} ---".format(func.__name__))
         result = func(*args, **kwargs)
         end = time()
         logging.info("--- EXECUTION TIME of {}: {} SECONDS ---".format(func.__name__, (end - start)))
