9bd245e0b00290682cff1abad0f9ddddd2b55b0e
pancham <panchamrb@phonepe.com>
2019-03-11 13:12:49 +0530

    Remove DDL and unused queries and env variables

--- app/common/constants_and_variables.py
+++ app/common/constants_and_variables.py
@@ -4,23 +4,6 @@ import os
 
 
 class AppConstants(object):
-    QUERY_CREATE_TABLE = '''create table strava_telegram_bot(
-        id serial NOT NULL,
-        athlete_id INTEGER PRIMARY KEY,
-        telegram_username VARCHAR NOT NULL,
-        name VARCHAR NOT NULL,
-        access_token VARCHAR NOT NULL,
-        refresh_token VARCHAR NOT NULL,
-        expires_at INTEGER NOT NULL,
-        active BOOLEAN NOT NULL DEFAULT TRUE,
-        strava_data json DEFAULT NULL,
-        update_indoor_ride BOOLEAN NOT NULL DEFAULT FALSE,
-        update_indoor_ride_data json DEFAULT NULL,
-        chat_id VARCHAR DEFAULT NULL,
-        enable_activity_summary BOOLEAN NOT NULL DEFAULT TRUE,
-        created timestamp NOT NULL,
-        updated timestamp default current_timestamp NOT NULL
-        );'''
 
     QUERY_INSERT_VALUES = "INSERT INTO strava_telegram_bot (athlete_id, name, access_token, refresh_token, " \
                           "expires_at, telegram_username, active, created) " \
@@ -51,13 +34,9 @@ class AppVariables(object):
     page_title = os.environ.get('PAGE_TITLE')
     secret_key = os.environ.get('SECRET_KEY')
     app_port = os.environ.get('APP_PORT')
-    database_url = os.environ.get('DATABASE_URL')
     app_debug = os.environ.get('APP_DEBUG')
     app_host = os.environ.get('APP_HOST')
     bot_url = os.environ.get('BOT_URL')
-    telegram_bot_token = os.environ.get('TELEGRAM_BOT_TOKEN')
-    shadow_mode = os.environ.get('SHADOW_MODE')
-    shadow_mode_chat_id = os.environ.get('SHADOW_MODE_CHAT_ID')
     scout_monitor = os.environ.get('SCOUT_MONITOR')
     scout_key = os.environ.get('SCOUT_KEY')
     scout_name = os.environ.get('SCOUT_NAME')
